25c5b237ac55561f6ab062209c12bed87f66b648
Alux6 <76573881+Alux6@users.noreply.github.com>
2021-01-31 11:41:38 +0100

    Now the code works faster.
    Now it works without any interruption as i removed the loops and used "if" statements instead.

--- detector_covid.ino
+++ detector_covid.ino
@@ -9,73 +9,83 @@
 DHT tempSens(dhtPin, dhtTipo);
 
 Adafruit_CCS811 Sensor;
-LiquidCrystal_I2C Pant(0x27,16,2);
-long TiempoDebug = 0;
-long TiempoDebug1 = 0;
-long Tiempo = 0;
-long Tiempo1 = 0;
-int Debug = 200;
-int Tmp = 5000;
-int CO2 = 1000;
-void setup() {
+LiquidCrystal_I2C Pant(0x27, 16, 2);
 
-  // put your setup code here, to run once:
-pinMode(11, OUTPUT);
-pinMode(10, OUTPUT);
-pinMode(9, OUTPUT);
-Serial.begin(9600);
-tempSens.begin();
-Sensor.begin();
-Pant.init();
-Pant.backlight();
+long TiempoDebug = 0;
+long Seg = 0;
+long Tiempo = 0;
+int Debug = 500;
+int Tmp = 5000;
+int CO2 = 0;
+int Part1 = 1;
+int Part2 = 0;
+float t = tempSens.readTemperature();
+
+void setup() {
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  Serial.begin(9600);
+  tempSens.begin();
+  Sensor.begin();
+  Pant.init();
+  Pant.backlight();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-float t = tempSens.readTemperature();
-if(((millis() - Tiempo) < Tmp)){
-  Pant.clear();
-  Pant.setCursor(0,0);
-  Pant.print("Tiempo transcur.");
-  while (((millis() - Tiempo) < Tmp)){
-    if((millis() - TiempoDebug) > Debug){
-      Pant.setCursor(0,1);
-      Pant.print((millis()/1000)+1);
+/*El tiempo y la concentración de CO2 sólo se actualiza cuando hay algún cambio, así ahorramos energía a la pila y tiempo al programa, 
+y evitamos el efecto de actualización continua de la pantalla que queda feísimo */
+  if ((millis() - Tiempo) <= Tmp) {
+    if (Part1 == 1) {
+      Pant.clear();
+      Pant.setCursor(0, 0);
+      Pant.print("Tiempo transcur.");
+      Part1 = 0;
+      Part2 = 1;
+    }
+    if ((millis() / 1000) != Seg) {
+      Pant.setCursor(0, 1);
+      Pant.print((millis() / 1000) + 1);
+      Seg = millis() / 1000;
       Pant.print(" Segundos");
-      TiempoDebug = millis();
     }
   }
-}
-if((millis()-TiempoDebug1)> Debug){
- t = tempSens.readTemperature();
- Serial.println(t);
- if(t <= 20){
-  digitalWrite(10, 0);
-  digitalWrite(11, 255);
-}
-else if((t > 20)&&(t<26)){
-  digitalWrite(10, 255);
-  digitalWrite(11, 255);
-}
-else{
-  digitalWrite(10, 255);
-  digitalWrite(11, 0);  
-}
-  TiempoDebug1 = millis();
-}
-if(((millis() - Tiempo) > Tmp)&&((millis() - Tiempo) < (2*Tmp))){
-    Pant.clear();
-    Pant.setCursor(0,0);
-    Pant.print("Calidad Aire");
-    while (((millis() - Tiempo) < (2* Tmp))){
-      if((millis() - TiempoDebug) > Debug){
-        Sensor.readData();
-        Pant.setCursor(0,1);
-        Pant.print(Sensor.geteCO2());
-        Pant.print(" PPM");
-        TiempoDebug = millis();
-      }
+/*sólo conservamos el Debug del sensor de temperatura*/
+  if ((millis() - TiempoDebug) > Debug) {
+    t = tempSens.readTemperature();
+    Serial.println(t);
+    if (t <= 20) {
+      digitalWrite(10, 0);
+      digitalWrite(11, 255);
+    }
+    else if ((t > 20) && (t < 26)) {
+      digitalWrite(10, 255);
+      digitalWrite(11, 255);
+    }
+    else {
+      digitalWrite(10, 255);
+      digitalWrite(11, 0);
+    }
+    TiempoDebug = millis();
   }
-  Tiempo = millis();
-}
+/* He sustituido todos los while para evitar bloqueo de código*/
+  if (((millis() - Tiempo) > Tmp) && ((millis() - Tiempo) < (2 * Tmp))) {
+    Sensor.readData();
+    Sensor.geteCO2();
+    if (Part2 == 1) {
+      Pant.clear();
+      Pant.setCursor(0, 0);
+      Pant.print("Calidad Aire");
+      Part1 = 1;
+      Part2 = 0;
+    }
+    if (CO2 != Sensor.geteCO2()) {
+      Pant.setCursor(0, 1);
+      CO2 = Sensor.geteCO2();
+      Pant.print(Sensor.geteCO2());
+      Pant.print(" PPM");
+    }
+  }
+  else if ((millis() - Tiempo) >= (2 * Tmp)) {
+    Tiempo = millis();
+  }
 }
